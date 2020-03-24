077d35cbd51acad8b06193e3a109f781ba780baf
Ana Hernandez <her17138@uvg.edu.gt>
2020-03-24 14:10:56 -0600

    Update pgAPI.js

--- api/routes/pgAPI.js
+++ api/routes/pgAPI.js
@@ -1,20 +1,22 @@
-import { Pool } from 'pg';
+var Pool = require('pg-pool');
 var express = require('express');
 var router = express.Router();
-import dotenv from 'dotenv';
+//require ('dotenv').config();
 
-dotenv.config();
-
-const databaseConfig = { connectionString: process.env.DATABASE_URL };
+const databaseConfig = { connectionString: "postgresql://postgres:postgres@52.14.98.239:5432/trackeat" };
 const pool = new Pool(databaseConfig);
-let connection_proof = "No conecto :/ chale";
-pool.on('connect',() =>{
+let connection_proof = "___No conecto :/ chale";
+pool.on('connect', client =>{
     connection_proof = "Si conecto soy el papa de Gustavo"
 })
-
 
 router.get("/",function(req, res, next){
     res.send(connection_proof);
 })
-
+/*pool.query('SELECT * FROM usuario', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})*/
+pool.end();
 module.exports = router;
+
