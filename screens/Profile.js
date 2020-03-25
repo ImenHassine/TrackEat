c13b9b737406d7ec83437d191f027651e573b7cd
Ana Hernandez <her17138@uvg.edu.gt>
2020-03-24 20:58:51 -0600

    profile .js
    inicio

--- screens/Profile.js
+++ screens/Profile.js
@@ -7,6 +7,8 @@ import { Icon } from '../components';
 import { Images, materialTheme } from '../constants';
 import { HeaderHeight } from "../constants/utils";
 
+//const pool = require("../api/routes/poolconfig.js");
+import pool from '../api/routes/poolconfig';
 const { width, height } = Dimensions.get('screen');
 const thumbMeasure = (width - 48 - 32) / 3;
 
@@ -16,7 +18,7 @@ export default class Profile extends React.Component {
       <Block flex style={styles.profile}>
         <Block flex>
           <ImageBackground
-            source={{uri: Images.Profile}}
+            source={{uri: Images.ProfileP}}
             style={styles.profileContainer}
             imageStyle={styles.profileImage}>
             <Block flex style={styles.profileDetails}>
@@ -24,9 +26,6 @@ export default class Profile extends React.Component {
                 <Text color="white" size={28} style={{ paddingBottom: 8 }}>Rachel Brown</Text>
                 <Block row space="between">
                   <Block row>
-                    <Block middle style={styles.pro}>
-                      <Text size={16} color="white">Pro</Text>
-                    </Block>
                     <Text color="white" size={16} muted style={styles.seller}>Seller</Text>
                     <Text size={16} color={materialTheme.COLORS.WARNING}>
                       4.8 <Icon name="shape-star" family="GalioExtra" size={14} />
@@ -35,7 +34,7 @@ export default class Profile extends React.Component {
                   <Block>
                     <Text color={theme.COLORS.MUTED} size={16}>
                       <Icon name="map-marker" family="font-awesome" color={theme.COLORS.MUTED} size={16} />
-                      {` `} Los Angeles, CA
+                      {` `} Guatemala, GT
                       </Text>
                   </Block>
                 </Block>
@@ -49,23 +48,19 @@ export default class Profile extends React.Component {
             <Block row space="between" style={{ padding: theme.SIZES.BASE, }}>
               <Block middle>
                 <Text bold size={12} style={{marginBottom: 8}}>36</Text>
-                <Text muted size={12}>Orders</Text>
+                <Text muted size={12}>Órdenes</Text>
               </Block>
               <Block middle>
                 <Text bold size={12} style={{marginBottom: 8}}>5</Text>
-                <Text muted size={12}>Bids & Offers</Text>
-              </Block>
-              <Block middle>
-                <Text bold size={12} style={{marginBottom: 8}}>2</Text>
-                <Text muted size={12}>Messages</Text>
+                <Text muted size={12}>Puntos</Text>
               </Block>
             </Block>
             <Block row space="between" style={{ paddingVertical: 16, alignItems: 'baseline' }}>
-              <Text size={16}>Recently viewed</Text>
-              <Text size={12} color={theme.COLORS.PRIMARY} onPress={() => this.props.navigation.navigate('Home')}>View All</Text>
+              <Text size={16}>Órdenes recientes</Text>
+              <Text size={12} color={theme.COLORS.PRIMARY} onPress={() => this.props.navigation.navigate('Home')}>Ver todas</Text>
             </Block>
             <Block style={{ paddingBottom: -HeaderHeight * 2 }}>
-              <Block row space="between" style={{ flexWrap: 'wrap' }} >
+              {/*<Block row space="between" style={{ flexWrap: 'wrap' }} >
                 {Images.Viewed.map((img, imgIndex) => (
                   <Image
                     source={{ uri: img }}
@@ -74,7 +69,7 @@ export default class Profile extends React.Component {
                     style={styles.thumb}
                   />
                 ))}
-              </Block>
+                </Block>*/}
             </Block>
           </ScrollView>
         </Block>
--- screens/SignIn.js
+++ screens/SignIn.js
@@ -23,7 +23,7 @@ export default class SignIn extends React.Component {
       });
       if (type === 'success') {
         const response = await fetch(`https://graph.facebook.com/me?access_token=${token}&fields=email,name`);
-        console.log('Logged in!', `Hi ${(await response.json()).name}!`);
+        console.log(await response.json());
       } else {
       }
     } catch ({ message }) {
