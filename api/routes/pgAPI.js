2ca473bb3d794254c5043f7733e0095238795fe6
Mafer <marialopez@pacifiko.com>
2020-04-22 19:03:33 -0600

    obtener orden de cliente, obtener nombre de productos, obtener puntos de cliente, obtener canjeables segun cantidad de puntos, hacer update a una orden del estado

--- api/routes/pgAPI.js
+++ api/routes/pgAPI.js
@@ -7,6 +7,8 @@ pool.on('connect', client =>{
     connection_proof = "Si conecto soy el papa de Gustavo x2"
 })
 
+
+//crea el nuevo usuario y guarda su informacion en la base de datos
 router.get("/email/:email/password/:password/image/:image/name/:name",function(req, res, next){
     next();
 }, function(req, res) {
@@ -40,8 +42,46 @@ router.get("/password/:password/email/:email",function(req, res, next){
   const params = req.params
   const email = params.email;
   const password = params.password;
-  const query = { text: 'SELECT id, email, name, image FROM usuario WHERE email = $1 and password = $2' ,
+  const query = { text: 'SELECT id, email, nombre, image FROM usuario WHERE email = $1 and password = $2' ,
   values: [email, password] }
+  const response = res;
+  pool.query(query, (err, res) => {
+    if(err) {
+      response.send(err.stack)
+    } else { 
+      response.send(res.rows[0])
+    }
+  })
+})
+
+//obtener los puntos de un usuario
+router.get("/userId/:userId",function(req, res, next){
+  next();
+}, function(req, res) {
+
+  const params = req.params
+  const userId = params.userId;
+  const query = { text: 'SELECT puntos FROM usuario WHERE id = $1' ,
+  values: [userId] }
+  const response = res;
+  pool.query(query, (err, res) => {
+    if(err) {
+      response.send(err.stack)
+    } else { 
+      response.send(res.rows[0])
+    }
+  })
+})
+
+//obtener canjeables dada la cantidad de puntos de un usuario
+router.get("/puntos/:puntos",function(req, res, next){
+  next();
+}, function(req, res) {
+
+  const params = req.params
+  const puntos = params.puntos;
+  const query = { text: 'SELECT descripcion FROM canjeables WHERE cantidad = $1' ,
+  values: [puntos] }
   const response = res;
   pool.query(query, (err, res) => {
     if(err) {
